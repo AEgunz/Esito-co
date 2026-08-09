20dfe80dc432800c378f3d39192630415e75c498
AEgunz <ayoub.drf@gmail.com>
2026-08-09 01:41:05 +0100

    Fix import names in reviewRoutes.ts

--- server/src/routes/reviewRoutes.ts
+++ server/src/routes/reviewRoutes.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import { createReview, getProductReviews, getAllReviews, deleteReview } from '../controllers/reviewController';
-import { protect, admin } from '../middlewares/auth';
+import { authenticate, authorizeAdmin } from '../middlewares/auth';
 
 const router = express.Router();
 
 router.post('/', createReview);
-router.get('/all', protect, admin, getAllReviews);
+router.get('/all', authenticate, authorizeAdmin, getAllReviews);
 router.get('/:productId', getProductReviews);
-router.delete('/:id', protect, admin, deleteReview);
+router.delete('/:id', authenticate, authorizeAdmin, deleteReview);
 
 export default router;
